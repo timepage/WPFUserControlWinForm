d8ea2341c08fe1f2cc184b3e7420ad5512814569
timp <tim.page@pipelogix.com>
2022-02-08 14:26:47 -0800

    Add project files.

---
+++ WPFUserControlWinForm/Form1.cs
@@ -0,0 +1,14 @@
+﻿using System.Windows.Forms;
+
+namespace WPFUserControlWinForm
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            elementHost1.Child = new UserControl1();
+        }
+    }
+}
---
+++ WPFUserControlWinForm/UserControl1.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Controls;
+
+namespace WPFUserControlWinForm
+{
+    public partial class UserControl1 : UserControl
+    {
+        public UserControl1()
+        {
+            InitializeComponent();
+        }
+    }
+}
